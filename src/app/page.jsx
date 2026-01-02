a5485359486dc45673005c799241c3e82a22c7f0
Aniket-1295 <aniketkamble1642002@gmail.com>
2026-01-02 22:46:47 +0530

    project-setup

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div className="p-4 text-2xl flex justify-center ">
+     <Button> Text</Button>
+    </div>
+  );
+}
