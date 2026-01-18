dc8d4ba0987690a4e68b7586c71a4de92c29753c
Aniket-1295 <aniketkamble1642002@gmail.com>
2026-01-18 10:07:25 +0530

    Authentication

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
     <div className="p-4 text-2xl flex justify-center ">
      <Button> Text</Button>
+     <UserButton />
     </div>
   );
 }
