498ab99597afe5dec4f03a7cec80b7b85ed797e8
Aniket-1295 <aniketkamble1642002@gmail.com>
2026-01-21 16:54:29 +0530

    AI-Jobs

--- src/inngest/functions.js
+++ src/inngest/functions.js
@@ -1,10 +1,25 @@
 import { inngest } from "./client";
+import { gemini, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
-  { event: "test/hello.world" },
+  { event: "agent/hello" },
+
+
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello world` };
+    
+   const helloAgent = createAgent({
+    name:"hello-agent",
+    description:"An agent that says hello",
+    system:"You're a friendly agent that greets people.",
+    model:gemini({model:"gemini-2.5-flash"}),
+   })
+
+   const {output} = await helloAgent.run("Hello, how are you?");
+
+   return{
+    message: output[0].content
+   }
   },
+
 );
