02fc4e10c2fb17bdbda308d4a346a2eb146ee31b
Aniket-1295 <aniketkamble1642002@gmail.com>
2026-01-22 18:45:16 +0530

    setUp E2B

--- src/inngest/functions.js
+++ src/inngest/functions.js
@@ -1,5 +1,7 @@
 import { inngest } from "./client";
 import { gemini, createAgent } from "@inngest/agent-kit";
+// import Sandbox from "@e2b/code-interpreter"
+// import { Sandbox } from "@e2b/sdk";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -7,7 +9,25 @@ export const helloWorld = inngest.createFunction(
 
 
   async ({ event, step }) => {
+
+    // const sandboxId = await step.run("get-sandbox-id",async()=>{
+
+    //   const sandbox = await Sandbox.create("v0-nextjs-dev")
+
+    //   return sandbox.sandboxId
+    // })
+
+    // const sandboxId = await step.run("get-sandbox-id", async () => {
+    //   const sandbox = await Sandbox.create({
+    //     template: "7cl2nt1ar3m08c01aoet",
+    //   });
+
+    //   return sandbox.id;
+    // });
+
+
     
+
    const helloAgent = createAgent({
     name:"hello-agent",
     description:"An agent that says hello",
@@ -17,8 +37,23 @@ export const helloWorld = inngest.createFunction(
 
    const {output} = await helloAgent.run("Hello, how are you?");
 
+  //  const sandboxUrl = await step.run("get-sandbox-url", async() => {
+  //   const sandbox = await Sandbox.connect(sandboxId);
+  //   const host = sandbox.getHost(3000);
+
+  //   return `http://${host}`;
+  // });
+
+  // const sandboxUrl = await step.run("get-sandbox-url", async () => {
+  //   const sandbox = await Sandbox.fromId(sandboxId);
+  //   const url = sandbox.getPortUrl(3000);
+
+  //   return url;
+  // });
+
    return{
-    message: output[0].content
+    message: output[0].content,
+    // sandboxUrl,
    }
   },
 
