e8ed2450dcce97e0d8b1f4ada07188f0638dd74f
Aniket-1295 <aniketkamble1642002@gmail.com>
2026-01-29 14:20:27 +0530

    Implement project and message models with CRUD operations; add React Query integration for data fetching

--- src/modules/home/components/ProjectsForm.jsx
+++ src/modules/home/components/ProjectsForm.jsx
@@ -14,7 +14,8 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Form, FormField } from "@/components/ui/form";
 import { PROJECT_TEMPLATES } from "@/constant";
-import { onInvoke } from "../actions";
+
+import { useCreateProject } from "@/modules/projects/hooks/project";
 
 
 const formSchema = z.object({
@@ -27,6 +28,7 @@ const formSchema = z.object({
 const ProjectsForm = () => {
   const [isFocused, setIsFocused] = useState(false);
   const router = useRouter();
+  const {mutateAsync , isPending} = useCreateProject()
 
   const form = useForm({
     resolver: zodResolver(formSchema),
@@ -40,29 +42,26 @@ const ProjectsForm = () => {
     form.setValue("content", prompt);
   };
 
-  const onSubmit = async (data) => {
+  const onSubmit = async (values) => {
     try {
-      console.log(data);
+      const res = await mutateAsync(values.content)
+      router.push(`/projects/${res.id}`)
+      toast.success("Project created successfully")
+      form.reset()
     } catch (error) {
       toast.error(error.message || "Failed to create project");
     }
   };
 
-  const onInvokeAI = async () => {
-    try {
-      const res = await onInvoke();
-      console.log(res);
-      toast.success("AI Agent Invoked Successfully");
-    } catch (error) {
-      toast.error(error.message || "Failed to create project");
-    }
-  };
+  const isButtonDisabled = isPending || !form.watch("content").trim()
+
+
 
   return (
     <div className="space-y-8">
       {/* Template Grid */}
 
-      <Button onClick={onInvokeAI}>Invoke AI Agent</Button>
+      
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3">
         {PROJECT_TEMPLATES.map((template, index) => (
@@ -137,8 +136,17 @@ const ProjectsForm = () => {
               </kbd>
               &nbsp; to submit
             </div>
-            <Button className={cn("size-8 rounded-full")} type="submit">
-              <ArrowUpIcon className="size-4" />
+            <Button
+            className={cn("size-8 rounded-full" , 
+              isButtonDisabled && "bg-muted-foreground border"
+            )}
+            disabled={isButtonDisabled}
+            type="submit"
+            >
+              {
+                isPending ? (<Spinner/>) : (<ArrowUpIcon className="size-4"/>)
+              }
+                
             </Button>
           </div>
         </form>
