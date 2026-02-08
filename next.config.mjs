3ca82237d01724e9cb9d593d4d0d7019ba2243c6
Aniket-1295 <aniketkamble1642002@gmail.com>
2026-02-08 10:14:09 +0530

    refactor: remove unused headers configuration from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-         
-        ]
-      }
-    ]
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
